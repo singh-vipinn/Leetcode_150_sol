2b7960f641544145de1477d8b65c606601507681
Vipin Singh <singhvipin123534@gmail.com>
2025-10-07 18:36:46 +0530

    Time: 0 ms (100%), Space: 43.2 MB (57.84%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,25 +2,24 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length, i=n-2;
 
-        //step find break point
-        while(i >=0 && nums[i] >= nums[i+1]) i--;
-
+        // step 1: find break point
+        while(i >= 0 && nums[i] >= nums[i+1] ) i--;
         if(i >= 0) {
-            // steps 2: find just lasger element
-            int j= n-1;
+            int j = n-1;
             while(nums[j] <= nums[i]) j--;
             swap(nums, i, j);
         }
-        // steps reverse right part
+
+        // reverse right part
         reverse(nums, i+1, n-1);
-        
     }
+
     private void swap(int[] nums, int i, int j) {
         int temp = nums[i];
-        nums[i] = nums[j];
+        nums[i]= nums[j];
         nums[j] = temp;
     }
     private void reverse(int[] nums, int i, int j) {
-        while(i<j) swap(nums, i++, j--);
-    }
+        while(i< j) swap(nums, i++, j--);
+            }
 }
