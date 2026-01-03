0533de207594f7a038954d34a234a782c54869be
Vipin Singh <singhvipin123534@gmail.com>
2026-01-03 20:45:29 +0530

    Time: 7 ms (63.09%), Space: 47.2 MB (49.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,24 +1,28 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+
+        // Create adjaency list
         for(int i=0; i<numCourses; i++) {
             graph.add(new ArrayList<>());
         }
 
-        int[] indegree = new int[numCourses];
-
-        for(int[] pre :prerequisites) {
-            int course = pre[0];
-            int prereq = pre[1];
-
+        // Build graph and indegree
+        for(int[] p: prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
             graph.get(prereq).add(course);
-            indegree[course]++;
+            inDegree[course]++;
         }
 
         Queue<Integer> queue = new LinkedList<>();
+
+        //add all coures with no prerequisites
         for(int i=0; i<numCourses; i++) {
-            if(indegree[i] == 0) 
-            queue.offer(i);
+            if(inDegree[i] == 0) {
+                queue.offer(i);
+            }
         }
 
         int completed = 0;
@@ -28,12 +32,12 @@ class Solution {
             completed++;
 
             for(int next : graph.get(curr)) {
-                indegree[next]--;
-                if(indegree[next] == 0) {
+                inDegree[next]--;
+                if(inDegree[next] == 0) {
                     queue.offer(next);
                 }
             }
         }
-        return completed ==numCourses;
+        return completed == numCourses;
     }
 }
